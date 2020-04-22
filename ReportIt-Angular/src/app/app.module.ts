34cc9bc48d44e31aabb8a3fcf146b252fb757376
Iván Morales <48416564+ivansmorales@users.noreply.github.com>
2020-04-21 19:41:05 -0500

    Added Admin-Reports Component
    Fixed error about mixing bootstrap and materialize.

--- ReportIt-Angular/src/app/app.module.ts
+++ ReportIt-Angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReportsComponent } from './main_components/reports/reports.component';
+import { ReportsAdminComponent } from './main_components/reports-admin/reports-admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReportsComponent
+    ReportsComponent,
+    ReportsAdminComponent,
   ],
   imports: [
     BrowserModule,
